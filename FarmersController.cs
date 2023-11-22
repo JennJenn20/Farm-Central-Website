1d81960258498b6ceb851c3dd90d5eb7e2e4abf6
JennJenn20 <94564864+JennJenn20@users.noreply.github.com>
2023-11-22 16:47:17 +0200

    Add files via upload

---
+++ FarmersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FarmingWebsite.Models;
+
+namespace FarmingWebsite.Controllers
+{
+    public class FarmersController : Controller
+    {
+        private AuthAppDBEntities db = new AuthAppDBEntities();
+
+        // GET: Farmers
+        public ActionResult Index()
+        {
+            return View(db.Farmers.ToList());
+        }
+        // GET: Farmers
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+
+        // GET: Farmers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Farmer farmer = db.Farmers.Find(id);
+            if (farmer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(farmer);
+        }
+
+        // GET: Farmers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Farmers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FarmerUsername,FarmerName,FarmerSurname,FarmerPassword")] Farmer farmer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Farmers.Add(farmer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(farmer);
+        }
+
+        // GET: Farmers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Farmer farmer = db.Farmers.Find(id);
+            if (farmer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(farmer);
+        }
+
+        // POST: Farmers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FarmerUsername,FarmerName,FarmerSurname,FarmerPassword")] Farmer farmer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(farmer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(farmer);
+        }
+
+        // GET: Farmers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Farmer farmer = db.Farmers.Find(id);
+            if (farmer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(farmer);
+        }
+
+        // POST: Farmers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Farmer farmer = db.Farmers.Find(id);
+            db.Farmers.Remove(farmer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ farmersProductsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FarmingWebsite.Models;
+
+namespace FarmingWebsite.Controllers
+{
+    [Authorize(Roles = "Farmer")]
+    public class farmersProductsController : Controller
+    {
+        
+        private AuthAppDBEntities db = new AuthAppDBEntities();
+
+        // GET: farmersProducts
+        public ActionResult Index()
+        {
+            var farmersProducts = db.farmersProducts.Include(f => f.Farmer).Include(f => f.Product);
+            return View(farmersProducts.ToList());
+        }
+
+        // GET: farmersProducts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            farmersProduct farmersProduct = db.farmersProducts.Find(id);
+            if (farmersProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(farmersProduct);
+        }
+
+        // GET: farmersProducts/Create
+        public ActionResult Create()
+        {
+            ViewBag.farmersUsername = new SelectList(db.Farmers, "FarmerUsername", "FarmerName");
+            ViewBag.productID = new SelectList(db.Products, "productID", "productName");
+            return View();
+        }
+
+        // POST: farmersProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "farmersProduct1,farmersUsername,productID")] farmersProduct farmersProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.farmersProducts.Add(farmersProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.farmersUsername = new SelectList(db.Farmers, "FarmerUsername", "FarmerName", farmersProduct.farmersUsername);
+            ViewBag.productID = new SelectList(db.Products, "productID", "productName", farmersProduct.productID);
+            return View(farmersProduct);
+        }
+
+        // GET: farmersProducts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            farmersProduct farmersProduct = db.farmersProducts.Find(id);
+            if (farmersProduct == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.farmersUsername = new SelectList(db.Farmers, "FarmerUsername", "FarmerName", farmersProduct.farmersUsername);
+            ViewBag.productID = new SelectList(db.Products, "productID", "productName", farmersProduct.productID);
+            return View(farmersProduct);
+        }
+
+        // POST: farmersProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "farmersProduct1,farmersUsername,productID")] farmersProduct farmersProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(farmersProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.farmersUsername = new SelectList(db.Farmers, "FarmerUsername", "FarmerName", farmersProduct.farmersUsername);
+            ViewBag.productID = new SelectList(db.Products, "productID", "productName", farmersProduct.productID);
+            return View(farmersProduct);
+        }
+
+        // GET: farmersProducts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            farmersProduct farmersProduct = db.farmersProducts.Find(id);
+            if (farmersProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(farmersProduct);
+        }
+
+        // POST: farmersProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            farmersProduct farmersProduct = db.farmersProducts.Find(id);
+            db.farmersProducts.Remove(farmersProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
